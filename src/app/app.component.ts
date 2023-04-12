96557830bf13406dcbeb491c42bedf41a42a69cc
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2023-04-12 10:13:44 -0500

    Integration of background and houses

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'atrapa-a-una-ave';
+  cantHouses = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 }
