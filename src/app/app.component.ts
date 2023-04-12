d772c36cbb2928458290e1d0fa605b450bd3783f
Milagros Marquina Mora <64057269+milagros-marquina-jumi@users.noreply.github.com>
2023-04-12 12:08:04 -0500

    Logical sum of scores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,21 @@ import { Bird } from './models/Bird';
 export class AppComponent implements OnInit {
   cantHouses = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   birds: Bird[] = []
-  scoreRosa: number = 0;
-  scoreAzul: number = 0;
+  scorePink: number = 0;
+  scoreBlue: number = 0;
   maxBird: number = 9;
   time: number = 0;
   previousRandom: number = 0;
   gameOver: Boolean = false;
+  sizeScore: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fnCreateBirds();
+  }
+
+  fnPlay() {
     this.fnInitialize();
     this.startTimer();
   }
@@ -48,6 +53,16 @@ export class AppComponent implements OnInit {
         break;
       }
     }
+
+    this.fnValidateIncreaseScore();
+  }
+
+  fnValidateIncreaseScore() {
+    const suma = this.scoreBlue + this.scorePink;
+
+    if (suma % 10 == 0 && suma > 0) {
+      this.sizeScore++;
+    }
   }
 
   fnCleanBirds() {
@@ -60,20 +75,32 @@ export class AppComponent implements OnInit {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
+  fnCaptureBird(item: Bird): any {
+    if (!this.gameOver) {
+      if (item.number! % 2 == 0) {
+        this.scorePink++;
+      } else {
+        this.scoreBlue++;
+      }
+    }
+  }
+
   fnInitialize() {
-    this.scoreRosa = 0;
-    this.scoreAzul = 0;
+    this.scorePink = 0;
+    this.scoreBlue = 0;
     this.time = 60;
-    this.gameOver = true;
+    this.gameOver = false;
     this.previousRandom = 0;
+    this.sizeScore = 16;
     this.fnCreateBirds();
   }
 
   fnDecreaseTime() {
     this.time--;
 
-    if (this.time < 0) {
-      this.gameOver = false;
+    if (this.time <= 0) {
+
+      this.gameOver = true;
     }
   }
 
